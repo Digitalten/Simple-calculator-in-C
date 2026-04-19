c0abdcf59772c006502270965d14a82b90e14caa
Digi <Caioecayna@gmail.com>
2026-04-19 04:49:05 -0300

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -1,18 +1,45 @@
 #include <stdio.h>
-int main(){
 
-    int a, b, c;
+int main() {
 
-   while(1) {
-    printf("\nPlease enter the desired multiplication: \n");
-    scanf("%d %d", &a, &b);
+    int option, a, b, c;
 
-    c = a*b;
+    printf("Choose a calculation expression between 1 for +, 2 for *, 3 for -, and 4 for /.\n");
+    scanf("%d", &option);
 
-    printf("\nResult: %d.\n", c);
+    printf("Please enter the desired numbers: \n");
+    scanf("%d %d", &a, &b);    
 
-   }
+    switch (option) {
+        case 1:
+            c = a + b;
+            printf("Result: %d", c);    
+            break;
 
-   return 0;
+        case 2:
+            c = a * b;
+            printf("Result: %d", c);    
+            break;
+
+        case 3:
+            c = a - b;
+            printf("Result: %d", c);    
+            break;
+
+        case 4:
+            if (b != 0) {
+            c = a / b;
+            printf("Result: %d", c);
+            } else {
+            printf("Cannot divide by zero. Sorry.\n");
+            }    
+            break;
+
+        default:
+        printf("Invalid option, please try again.");
+
+    }
+
+    return 0;
 
 }

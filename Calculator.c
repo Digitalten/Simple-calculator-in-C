ab2f3cbb2198584815f8bf4e51dac42e59fb87d4
Digi <Caioecayna@gmail.com>
2026-04-20 06:11:43 -0300

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -3,7 +3,9 @@
 int main() {
 
     int option, a, b, c;
+    float f;
 
+    while (1) {
     printf("Choose a calculation expression between 1 for +, 2 for *, 3 for -, and 4 for /.\n");
     scanf("%d", &option);
 
@@ -13,35 +15,35 @@ int main() {
     switch (option) {
         case 1:
             c = a + b;
-            printf("Result: %d", c);    
+            printf("Result: %d\n", c);    
             break;
 
         case 2:
             c = a * b;
-            printf("Result: %d", c);    
+            printf("Result: %d\n", c);    
             break;
 
         case 3:
             c = a - b;
-            printf("Result: %d", c);    
+            printf("Result: %d\n", c);    
             break;
 
         case 4:
             if (b != 0) {
-            c = a / b;
-            printf("Result: %d", c);
+            f = (float)a / b;
+            printf("Result: %.2f\n", f);
             } else {
             printf("Cannot divide by zero. Sorry.\n");
             }    
             break;
 
         default:
-        printf("Invalid option, please try again.");
+        printf("Invalid option, please try again.\n");
+
+        }
 
     }
 
-    getchar();
-    getchar();
     return 0;
 
 }

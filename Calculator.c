51fac370e2d2ad5287643a2274835ebe1b2dc44e
Digi <Caioecayna@gmail.com>
2026-05-15 02:20:49 -0300

    Update Calculator.c

--- Calculator.c
+++ Calculator.c
@@ -1,49 +1,65 @@
 #include <stdio.h>
 
-int main() {
-
-    int option, a, b, c;
-    float f;
-
-    while (1) {
-    printf("Choose a calculation expression between 1 for +, 2 for *, 3 for -, and 4 for /.\n");
-    scanf("%d", &option);
-
-    printf("\nPlease enter the desired numbers: \n");
-    scanf("%d %d", &a, &b);    
-
-    switch (option) {
-        case 1:
-            c = a + b;
-            printf("Result: %d\n", c);    
-            break;
-
-        case 2:
-            c = a * b;
-            printf("Result: %d\n", c);    
-            break;
-
-        case 3:
-            c = a - b;
-            printf("Result: %d\n", c);    
-            break;
-
-        case 4:
-            if (b != 0) {
-            f = (float)a / b;
-            printf("Result: %.2f\n", f);
-            } else {
-            printf("Cannot divide by zero. Sorry.\n");
-            }    
-            break;
-
-        default:
-        printf("Invalid option, please try again.\n");
-
-        }
-
+void menu(){
+    printf("1 - Plus\n");
+    printf("2 - Minus\n");
+    printf("3 - Multiplication\n");
+    printf("4 - Division\n");
+    printf("5 - Exit\n");
     }
 
-    return 0;
+    int plus(int a, int b){
+        return a + b;
+    }
 
-}
+    int minus(int a, int b){
+        return a - b;
+    }
+
+    int multiplication(int a, int b){
+        return a * b;
+    }
+
+    float division(int a, int b){
+        if(b == 0){
+            printf("Cannot divide by 0.\n");
+            return 0;
+        }
+        return (float)a / b;
+    }
+
+    int main(){
+        int option;
+        
+        do {
+            menu();
+            scanf("%d", &option);
+
+            if (option == 1) {
+                printf("The desired calculation please: \n");
+                int x, y;
+                scanf("%d %d", &x, &y);
+                printf("Result: %d\n\n", plus(x, y));
+            }
+            if (option == 2) {
+                printf("The desired calculation please: \n");
+                int x, y;
+                scanf("%d %d", &x, &y);
+                printf("Result: %d\n\n", minus(x, y));
+            }
+            if (option == 3) {
+                printf("The desired calculation please: \n");
+                int x, y;
+                scanf("%d %d", &x, &y);
+                printf("Result: %d\n\n", multiplication(x, y));
+            }
+            if (option == 4) {
+                printf("The desired calculation please: \n");
+                int x, y;
+                scanf("%d %d", &x, &y);
+                printf("Result: %f\n\n", division(x, y));
+            }
+        } while (option != 5);
+
+        return 0;
+    }
